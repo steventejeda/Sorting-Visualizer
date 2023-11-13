3a578d791f3fcd1a7a9e31900e819907c6bb93d9
Steven Tejeda <steventejeda@Stevens-MacBook-Pro.local>
2023-11-13 17:55:03 -0500

    Added sorting controls

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 class DrawInformation:
@@ -14,6 +15,10 @@ class DrawInformation:
         (160, 160, 160),
         (192, 192, 192)
     ]
+
+    FONT_NAME = 'comicsans'
+    FONT = pygame.font.SysFont(FONT_NAME, 24)
+    LARGE_FONT = pygame.font.SysFont(FONT_NAME, 40)
 
     SIDE_PAD = 100
     TOP_PAD = 150
@@ -32,19 +37,27 @@ class DrawInformation:
         self.min_val = min(lst)
 
         self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
-        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.block_height = math.floor((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
         self.start_x = self.SIDE_PAD // 2
 
 
 def draw(draw_info):
     draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+ 
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2 , 45))
+
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2 , 75))
+
     draw_list(draw_info)
     pygame.display.update()
+
 
 def draw_list(draw_info):
     lst = draw_info.lst
 
-    for i , val in enumerate(lst):
+    for i, val in enumerate(lst):
         x = draw_info.start_x + i * draw_info.block_width
         y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
 
@@ -65,15 +78,17 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    n = 50 
+    num = 50 
     min_val = 0
     max_val = 100
 
-    lst = generate_starting_list(n, min_val, max_val)
+    lst = generate_starting_list(num, min_val, max_val)
     draw_info = DrawInformation(800, 600, lst)
+    sorting = False
+    ascending = True
 
     while run: 
-        clock.tick(60)
+        clock.tick(60) 
         draw(draw_info)
 
         pygame.display.update()
@@ -88,6 +103,13 @@ def main():
             if event.key == pygame.K_r:
                 lst = generate_starting_list(n, min_val, max_val)
                 draw_info.set_list(lst)
+                sorting = False
+            elif event.key == pygame.K_SPACE and sorting == False: 
+                sorting = True
+            elif event.key == pygame.K_a and not sorting:
+                ascending = True
+            elif event.key == pygame.K_d and not sorting:
+                ascending = False
     
     pygame.quit()
 
